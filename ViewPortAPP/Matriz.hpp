dba5e450dfce8d432b108545b85b5e4676205667
jonatan <jonatan@jonatan-VirtualBox>
2017-03-22 01:28:02 -0300

    Classe Matriz reestruturada, mas ainda não multiplica.

--- ViewPortAPP/Matriz.hpp
+++ ViewPortAPP/Matriz.hpp
@@ -14,56 +14,60 @@
 #ifndef MATRIZ_HPP
 #define MATRIZ_HPP
 
-#include "ListaEnc.hpp"
+#include <vector>
+
+using namespace std;
 
 class Matriz {
 private:
-    int l, c;
-    ListaEnc<ListaEnc<float>*>* matriz;
+    int num_lin, num_col;
+    //Os valores da matriz são armazeados linearmente em um único vetor
+    std::vector<float> valores;
+    
 
 public:
 
-    Matriz(int l, int c) : l(l), c(c) {
-        matriz = new ListaEnc<ListaEnc<float>*>();
-        for (int i = 0; i < l; i++) {
-            matriz->adiciona(new ListaEnc<float>());
-            for (int j = 0; j < c; j++) {
-                matriz->get(i)->adiciona(0);
-            }
-        }
+    Matriz(int linhas, int colunas){
+        num_lin = linhas;
+        num_col = colunas;
+        valores->assign(num_lin*num_col, 0.0);
     }
-
-    int col() {
-        return c;
+    
+    /*Cada linha inteira possui [nº colunas] valores, então o índice
+     é calculado pelo número de 'linhas cheias' no vetor mais o número
+     da coluna desejada. O valor é subtraido em 1 pois o vector conta
+     as posições a partir de 0.*/
+    float get(int l, int c){
+        return valores[(l-1)*num_col + c-1];
     }
-
-    int lin() {
-        return l;
+    
+    float set(int l, int c, float dado){
+        valores[(l-1)*num_col + c-1] = dado;
     }
-
-    float get(int l, int c) {
-        return matriz->get(l)->get(c);
+    
+    int lin(){
+        return num_lin;
     }
-
-    void set(int l, int c, float dado) {
-        matriz->get(l)->modifica(c, dado);
+    
+    int col(){
+        return num_col;
     }
-
-    Matriz* operator*(Matriz* m) {
-        Matriz* r = new Matriz(l, m->col());
-        for (int i = 0; i < l; i++) {
-            for (int j = 0; j < m->col(); j++) {
-                float a = 0;
-                for (int k = 0; k < m->l; k++) {
-                    for (int n = 0; n < m->col(); n++) {
-                        a += get(k,n) * m->get(n,k);
-                    }
+    
+    Matriz* operator*(Matriz* m){
+        Matriz res = new Matriz(num_lin, m->col());
+        for(int l = 1; l =< res->lin(); l++){
+            for(int c = 1; c =< res->col(); c++){
+                float dado_novo = 0.0;
+                for(int i = 0; i < num_lin; i++){
+                    dado_novo += this->get(l, c+i) * m->get(l+i, c); //wrong
                 }
-                r->set(i,j,a);
+                res->set(l, c, dado_novo);
             }
+            
         }
-        return r;
+        return res;
     }
+
 };
 
 #endif /* MATRIZ_HPP */
